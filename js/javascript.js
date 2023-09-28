651d9b1e120cdb78191cfc35eeb8ab46a2eccd83
KingTorres <145770759+KingTorres@users.noreply.github.com>
2023-09-29 01:38:13 +0800

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -8,10 +8,11 @@ function content1(contentTrigger) //content changer and navbar interface
             break;
         case '2':
             selectednav(selval)
-            RepeatedWelcomeLoop()
+            Skill_Item_Animation_Enable()
             break;
         case '3':
             selectednav(selval)
+            Background_Item_Animation_Enable()
             break;
         default:
             contentTrigger = 0;
@@ -26,25 +27,56 @@ function content1(contentTrigger) //content changer and navbar interface
             }
             document.getElementById('nav'+[trigger]).setAttribute("style","font-size: medium; font-weight: bold; color: #ffffff;")
             document.getElementById('div'+[trigger]).style.display = "block";
+            nav_hover('2');
     }
 }
 function kmbt(mousetrg)
 {
     if (mousetrg == 1)
     {
-        document.getElementById('mt').style.display = "block";
+        document.getElementById('mt').setAttribute("style","display: block; position: relative; z-index: 2;")
+        document.getElementById('mt').setAttribute("class","CallCard pull-right")
     }
     else{
-        document.getElementById('mt').style.display = "none";
+        document.getElementById('mt').setAttribute("style","display: none;")
     }
 }
 const sleepNow = (king) => new Promise((resolve) => setTimeout(resolve, king)) //Promise method
-async function RepeatedWelcomeLoop() {
+async function Skill_Item_Animation_Enable() {
     for (let i = 1; i <= 3; i++) {
         document.getElementById('skill_'+[i]).style.display = "none";
     }
     for (let i = 1; i <= 3; i++) {
         await sleepNow(500)
         document.getElementById('skill_'+[i]).style.display = "block";
+    }
+}
+async function Background_Item_Animation_Enable() {
+    for (let i = 1; i <= 3; i++) {
+        document.getElementById('Background_'+[i]).style.display = "none";
+    }
+    for (let i = 1; i <= 3; i++) {
+        await sleepNow(500)
+        document.getElementById('Background_'+[i]).style.display = "block";
+    }
+}
+
+
+function nav_hover(navval)
+{
+    if(navval == 1)
+    {
+        document.getElementById('formHeader').setAttribute("class","slidedown breadcrumb")
+        document.getElementById('navopen').style.display = "none";
+        document.getElementById('navclose').setAttribute("class","navbar_hover slidedown")
+        document.getElementById('navclose').setAttribute("style","display: block; margin-top: -21px;")
+        document.getElementById('bodyDiv').style.paddingTop = "60px";
+    }
+    else{
+        document.getElementById('formHeader').setAttribute("class","slideup breadcrumb")
+        document.getElementById('navclose').style.display = "none";
+        document.getElementById('navopen').setAttribute("class","navbar_hover slideup")
+        document.getElementById('navopen').setAttribute("style","display: block; margin-top: -21px;")
+        document.getElementById('bodyDiv').style.paddingTop = "20px";
     }
 }
